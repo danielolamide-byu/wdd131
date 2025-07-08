439bc143bdbdcd2a8fc85846d12aae07e4132a5c
danielolamide-byu <olamidedaniel874@gmail.com>
2025-07-08 02:15:29 +0100

    Homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const getYear = new Date();
+const currentYear = getYear.getFullYear();
+
+document.getElementById('currentyear').textContent = currentYear;
+document.getElementById('lastModified').textContent = document.lastModified;
+
+
